15b0eec6b91c60d3f55c8af4c105d3814f66973b
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-04 23:35:10 -0300

    fix(models/tournaments): remove winner field

--- tournaments/models.py
+++ tournaments/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
-from teams.models import Team
-
 
 class Tournament(models.Model):
     phase = models.CharField(max_length=100, null=True)
     finished = models.BooleanField(null=False, default=False)
-    winner = models.OneToOneField(
-        Team,
-        on_delete=models.CASCADE,
-        related_name='winner')
