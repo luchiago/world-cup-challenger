e551ea2ec306b9acd0546caa0e51049e8e6b868d
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-04 23:36:45 -0300

    fix: delete old api app

--- api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Team(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
-    points = models.IntegerField(default=0)
-    goals = models.IntegerField(default=0)
-    position = models.IntegerField(default=1)
-    
