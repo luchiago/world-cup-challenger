739c5071d1ba19995a23cd4a225d18cdb29a8b60
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-05 11:11:04 -0300

    feat: create input of teams endpoint

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from groups.services import CreateGroupService
+from tournaments.models import Tournament
+from tournaments.serializer import TournamentSerializer
+
+from .services import CreateTeamService
+
+
+class TeamList(APIView):
+    def post(self, request, format=None):
+        try:
+            tournament = Tournament()
+            teams = CreateTeamService(request).perform()
+            CreateGroupService(teams, tournament).perform()
+            tournament.phase = 'First Phase'
+            tournament.save()
+            serialized = TournamentSerializer(tournament)
+            return Response(
+                data=serialized.data,
+                status=status.HTTP_201_CREATED)
+        except Exception as e:
+            data = {'message': str(e)}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
