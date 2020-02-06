381fa15c869e92f197e32a466507e79ea0bbd725
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-06 10:47:24 -0300

    feat: create read state endpoint and related spec

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from matches.services import MatchService
+
+from .models import Tournament
+from .serializer import TournamentSerializer
+from .services import TournamentService
+
+
+@api_view(['GET'])
+def tournament_list(request):
+    current_tournament = Tournament.objects.last()
+    if not current_tournament:
+        data = {'phase': current_tournament}
+        return Response(data=data, status=status.HTTP_404_NOT_FOUND)
+    serialized = TournamentSerializer(current_tournament)
+    return Response(data=serialized.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def tournament_teams_list(request):
+    current_tournament = Tournament.objects.last()
+    if not current_tournament:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serialized = TournamentService(current_tournament).perform()
+    return Response(data=serialized.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def tournament_matches_list(request):
+    current_tournament = Tournament.objects.last()
+    if not current_tournament:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    matches = MatchService(current_tournament).perform()
+    return Response(data=matches, status=status.HTTP_200_OK)
