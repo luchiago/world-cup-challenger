815da60c3b0355d60f3b9fe52b2ab3dcdf6996e4
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-06 08:48:28 -0300

    feat(models/Match): add field phase to Match model

--- matches/models.py
+++ matches/models.py
@@ -5,6 +5,19 @@ from tournaments.models import Tournament
 
 
 class Match(models.Model):
+
+    FIRST_PHASE = 0
+    SECOND_PHASE = 1
+    SEMI_FINAL = 2
+    FINAL = 3
+
+    PHASE_CHOICES = [
+        (FIRST_PHASE, 'First Phase'),
+        (SECOND_PHASE, 'Second Phase'),
+        (SEMI_FINAL, 'Semifinals'),
+        (FINAL, 'Final')
+    ]
+
     away_team = models.ForeignKey(
         Team,
         related_name='away_team',
@@ -16,6 +29,11 @@ class Match(models.Model):
     away_team_goals = models.IntegerField(default=0)
     home_team_goals = models.IntegerField(default=0)
     played = models.BooleanField(default=False)
+    phase = models.CharField(
+        max_length=25,
+        default=None,
+        choices=PHASE_CHOICES,
+        null=True)
     tournament = models.ForeignKey(
         Tournament,
         related_name='matches',
