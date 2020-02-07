38f5490b275e6f960bbb5bc833c6a935efb70659
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-02-07 01:48:01 -0300

    feat(urls): add input matches results endpoint in urls

--- worldcup/urls.py
+++ worldcup/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^teams/', include(('teams.urls', 'teams'), namespace='teams')),
     url(r'^tournaments/', include(('tournaments.urls', 'tournaments'), namespace='tournaments')),
+    url(r'^matches/', include(('matches.urls', 'matches'), namespace='matches')),
 
 ]
